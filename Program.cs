105eb714c50f2cf5c75f3b8174e286c43e4395b1
mabbuprem <mabbupremkumar@gmail.com>
2022-02-23 00:09:53 +0530

    first position

--- Program.cs
+++ Program.cs
@@ -3,30 +3,27 @@ namespace genericProblems
 {
     class Program
     {
+
         public static void Main()
         {
             Console.WriteLine("Welcome to the generic problems");
             Program findMax = new Program();
-            Console.WriteLine($"Maximum number is {findMax.maxLength(569, 478, 888)}");
-
-
+            findMax.maxLength("Apple", "mango", "Banana");
         }
-        private int maxLength(int value1, int value2, int value3)
+        private void maxLength(string value1, string value2, string value3)
         {
             if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0)
             {
-                return value1;
+                Console.WriteLine(value1);
             }
             else if (value2.CompareTo(value3) > 0 && value2.CompareTo(value1) > 0)
             {
-                return value2;
+                Console.WriteLine(value2);
             }
             else
             {
-                return value3;
+                Console.WriteLine(value3);
             }
         }
     }
-
-
 }
