8a09e9ee8f0ad832d19b1921ac472380914dc653
mabbuprem <mabbupremkumar@gmail.com>
2022-02-22 23:47:56 +0530

    Add project files.

---
+++ IntMax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finding_Max_of_Generic_Topic
+{
+    internal class IntMax
+    {
+        public int value1;
+        public int value2;
+        public int value3;
+
+        public IntMax(int value1, int value2, int value3)
+        {
+            this.value1 = value1;
+            this.value2 = value2;
+            this.value3 = value3;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace genericProblems
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to the generic problems");
+            Program findMax = new Program();
+            Console.WriteLine($"Maximum number is {findMax.maxLength(569, 478, 888)}");
+
+
+        }
+        private int maxLength(int value1, int value2, int value3)
+        {
+            if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0)
+            {
+                return value1;
+            }
+            else if (value2.CompareTo(value3) > 0 && value2.CompareTo(value1) > 0)
+            {
+                return value2;
+            }
+            else
+            {
+                return value3;
+            }
+        }
+    }
+
+
+}
